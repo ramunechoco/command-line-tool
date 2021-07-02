e1ee336db7b88318815bc1d26fb3f17a3d876784
unknown <13518105@std.stei.itb.ac.id>
2021-07-02 14:21:27 +0700

    add more

--- mytool/mytool.py
+++ mytool/mytool.py
@@ -1,15 +1,13 @@
 import argparse 
 import os
+import json
 
 def main(): 
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfile', type=str, help='Source file path.', action='store')
-    parser.add_argument('-t', type=str, dest='format', help='Format to convert (either json or text) with plaintext default.', action='store', default='text') 
+    parser.add_argument('-t', type=str, dest='format', help="Format to convert (either 'json' or 'text') with plaintext default.", action='store', default='text') 
     parser.add_argument('-o', type=str, dest='destination', help='Destination file path. If not provided, a new file with similar name is created', action='store') 
     args = parser.parse_args()
-    print(args.inputfile)
-    print(args.destination)
-    print(args.format)
     handleFile(args.inputfile, args.destination, args.format)
 
 def handleFile(source, destination, format):
@@ -21,6 +19,25 @@ def handleFile(source, destination, format):
     else:
         form = ".txt" if (format == "text") else ".json"
         dest = destination if destination is not None else source + form
-        with open(source,'r') as firstfile, open(dest,'w') as secondfile:
-            for line in firstfile:
-                    secondfile.write(line)
+        if not (dest.lower().endswith(form)):
+            print("Error with destination file extension, please name according to the intended format.")
+        else:
+            if (format == "text"):
+                copyTextFile(source, dest)
+            else:
+                copyJSONFile(source, dest)
+
+def copyTextFile(source, destination):
+    with open(source,'r') as firstfile, open(destination,'w') as secondfile:
+        for line in firstfile:
+            secondfile.write(line)
+
+def copyJSONFile(source, destination):
+    i = 1
+    result = {}
+    with open(source,'r') as firstfile, open(destination,'w') as secondfile:
+        lines = firstfile.readlines()
+        for line in lines:
+            result[i] = line
+            i += 1
+        json.dump(result, secondfile)
